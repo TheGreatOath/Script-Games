c2e211ff9066d18dced2a4db54e1d7713366c2e4
TheGreatOath <pertuisance@gmail.com>
2025-08-18 16:45:09 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,1 +1,10 @@
+local games = {
+    [{17574618959}] = "https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/loader.lua",
+}
 
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
