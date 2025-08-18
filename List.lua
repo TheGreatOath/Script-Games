13a77ad70bc72949e543ae5706fbfc1eaff7d752
TheGreatOath <pertuisance@gmail.com>
2025-08-18 19:49:32 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,8 +1,16 @@
-local GAME_SCRIPTS = {
+-- I am desperate
+pcall(function()
+    local inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+    inviter.Join("https://discord.gg/97aFp97Hfr")
+end)
+
+local games = {
     [{17574618959}] = "https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/loader.lua",
 }
 
-if not GAME_SCRIPTS[game.placeId] then
-    Players.LocalPlayer:Kick("Worm doesn't support this game | Join our discord for more information")
-    return
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
 end
