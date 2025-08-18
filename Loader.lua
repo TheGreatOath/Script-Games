05ecd3d6bee1eedc90c74dd19d249eb751f6ae06
TheGreatOath <pertuisance@gmail.com>
2025-08-18 20:37:19 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,15 @@
+pcall(function()
+    local inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Utilities/refs/heads/main/Inviter.lua"))()
+    inviter.Join("https://discord.gg/97aFp97Hfr")
+end)
+
+local games = {
+    [{17574618959}] = "https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/loader.lua",
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
