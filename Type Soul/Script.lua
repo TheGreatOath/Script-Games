7d4656751165ac9251ea274797f952efc91ae2a5
TheGreatOath <pertuisance@gmail.com>
2025-08-18 21:59:58 +0300

    Update Script.lua

--- Type Soul/Script.lua
+++ Type Soul/Script.lua
@@ -1,11 +1,10 @@
-local Worm = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/main.lua"))() --Script For The Windows To Work 
+local Worm = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/InterfaceManager.lua"))()
-local Player = game.Players.LocalPlayer
 
 local Window = Worm:CreateWindow({
     Title = "Worm",
-    SubTitle = "",
+    SubTitle = "- TYPE://SOUL",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
@@ -13,44 +12,41 @@ local Window = Worm:CreateWindow({
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
-function MakeScriptHub() 
-  print("ASDDSFDSFFDSFDS")
-end
-
+--https://lucide.dev/icons/
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "home" }),
+    Player = Window:AddTab({ Title = "Player", Icon = "user" }),
+    ESP = Window:AddTab({ Title = "ESP", Icon = "eye" }),
+    Parry = Window:AddTab({ Title = "Parry", Icon = "shield" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
 local Options = Worm.Options
 
 do
+    local Section = Tabs.Main:AddSection("Section", "apple") -- Create section with icon
 
-Worm:Notify({
-        Title = "Logged In!",
-        Content = "Successfully Loaded Pls Enter The Key",
-        Duration = 2
+
+  
+    Worm:Notify({
+        Title = "Notification",
+        Content = "This is a notification",
+        SubContent = "SubContent", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
     })
 
-getgenv().Key = "Password"
-getgenv().KeyInput = "string"
 
-local Input = Tabs.Main:AddInput("Input", {
-        Title = "Enter key",
-        Default = "",
-        Placeholder = "...",
-        Numeric = false,
-        Finished = true,
-        Callback = function(txt)
-            getgenv().KeyInput = txt
-        end
+
+    Tabs.Main:AddParagraph({
+        Title = "Paragraph",
+        Content = "This is a paragraph.\nSecond line!"
     })
 
 
 
     Tabs.Main:AddButton({
-        Title = "Check key",
-        Description = "",
+        Title = "Button",
+        Description = "Very important button",
         Callback = function()
             Window:Dialog({
                 Title = "Title",
@@ -59,45 +55,13 @@ local Input = Tabs.Main:AddInput("Input", {
                     {
                         Title = "Confirm",
                         Callback = function()
-                        
-                                    if getgenv().KeyInput == getgenv().Key then
-                                    
-                                    Worm:Notify({
-    Title = "Checking Key",
-    Content = "Please Wait",
-    Duration = 3
-})
-wait(3)
-Worm:Notify({
-    Title = "Correct Key",
-    Content = "The Script Will Load In A Few Seconds.",
-    Duration = 2
-})
-wait(2)
-Worm:Destroy()
-wait(1)
-MakeScriptHub()
-
-else
-                            Worm:Notify({
-    Title = "Checking Key",
-    Content = "Please Wait",
-    Duration = 1
-})
-wait(1)
-Worm:Notify({
-    Title = "Incorrect Key",
-    Content = "Please Get Key The Key.",
-    Duration = 2
-})
+                            print("Confirmed the dialog.")
                         end
-                                end
-
                     },
                     {
                         Title = "Cancel",
                         Callback = function()
-
+                            print("Cancelled the dialog.")
                         end
                     }
                 }
@@ -105,23 +69,181 @@ Worm:Notify({
         end
     })
 
-    SaveManager:SetLibrary(Worm)
-    InterfaceManager:SetLibrary(Worm)
-    SaveManager:IgnoreThemeSettings()
-    SaveManager:SetIgnoreIndexes({})
-    InterfaceManager:SetFolder("WormScriptHub")
-    SaveManager:SetFolder("WormScriptHub/specific-game")
-
-    InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-    SaveManager:BuildConfigSection(Tabs.Settings)
 
 
-    Window:SelectTab(1)
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
 
-    Worm:Notify({
-        Title = "Worm",
-        Content = "The script has been loaded.",
-        Duration = 8
+    Toggle:OnChanged(function()
+        print("Toggle changed:", Options.MyToggle.Value)
+    end)
+
+    Options.MyToggle:SetValue(false)
+
+
+    
+    local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Slider",
+        Description = "This is a slider",
+        Default = 2,
+        Min = 0,
+        Max = 5,
+        Rounding = 1,
+        Callback = function(Value)
+            print("Slider was changed:", Value)
+        end
     })
-    SaveManager:LoadAutoloadConfig()
+
+    Slider:OnChanged(function(Value)
+        print("Slider changed:", Value)
+    end)
+
+    Slider:SetValue(3)
+
+
+
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+        Title = "Dropdown",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = false,
+        Default = 1,
+    })
+
+    Dropdown:SetValue("four")
+
+    Dropdown:OnChanged(function(Value)
+        print("Dropdown changed:", Value)
+    end)
+
+
+    
+    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
+        Title = "Dropdown",
+        Description = "You can select multiple values.",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = true,
+        Default = {"seven", "twelve"},
+    })
+
+    MultiDropdown:SetValue({
+        three = true,
+        five = true,
+        seven = false
+    })
+
+    MultiDropdown:OnChanged(function(Value)
+        local Values = {}
+        for Value, State in next, Value do
+            table.insert(Values, Value)
+        end
+        print("Mutlidropdown changed:", table.concat(Values, ", "))
+    end)
+
+
+
+    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
+        Title = "Colorpicker",
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    Colorpicker:OnChanged(function()
+        print("Colorpicker changed:", Colorpicker.Value)
+    end)
+    
+    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+
+
+    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
+        Title = "Colorpicker",
+        Description = "but you can change the transparency.",
+        Transparency = 0,
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    TColorpicker:OnChanged(function()
+        print(
+            "TColorpicker changed:", TColorpicker.Value,
+            "Transparency:", TColorpicker.Transparency
+        )
+    end)
+
+
+
+    local Keybind = Tabs.Main:AddKeybind("Keybind", {
+        Title = "KeyBind",
+        Mode = "Toggle", -- Always, Toggle, Hold
+        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+        -- Occurs when the keybind is clicked, Value is `true`/`false`
+        Callback = function(Value)
+            print("Keybind clicked!", Value)
+        end,
+
+        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+        ChangedCallback = function(New)
+            print("Keybind changed!", New)
+        end
+    })
+
+    -- OnClick is only fired when you press the keybind and the mode is Toggle
+    -- Otherwise, you will have to use Keybind:GetState()
+    Keybind:OnClick(function()
+        print("Keybind clicked:", Keybind:GetState())
+    end)
+
+    Keybind:OnChanged(function()
+        print("Keybind changed:", Keybind.Value)
+    end)
+
+    task.spawn(function()
+        while true do
+            wait(1)
+
+            -- example for checking if a keybind is being pressed
+            local state = Keybind:GetState()
+            if state then
+                print("Keybind is being held down")
+            end
+
+            if Worm.Unloaded then break end
+        end
+    end)
+
+    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Input",
+        Default = "Default",
+        Placeholder = "Placeholder",
+        Numeric = false, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+            print("Input changed:", Value)
+        end
+    })
+
+    Input:OnChanged(function()
+        print("Input updated:", Input.Value)
+    end)
 end
+
+SaveManager:SetLibrary(Worm)
+InterfaceManager:SetLibrary(Worm)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("WormScriptHub")
+SaveManager:SetFolder("WormScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Worm:Notify({
+    Title = "Worm",
+    Content = "The script has been loaded.",
+    Duration = 8
+})
+
+SaveManager:LoadAutoloadConfig()
