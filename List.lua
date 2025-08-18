523688137c43b01d187519d1fa6734165855a1b0
TheGreatOath <pertuisance@gmail.com>
2025-08-18 16:54:15 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,10 +1,8 @@
-local games = {
+local GAME_SCRIPTS = {
     [{17574618959}] = "https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/loader.lua",
 }
 
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        break
-    end
+if not GAME_SCRIPTS[game.placeId] then
+    Players.LocalPlayer:Kick("Scylla doesn't support this game | Join our discord for more information")
+    return
 end
