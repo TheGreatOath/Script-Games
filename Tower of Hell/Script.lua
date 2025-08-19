bc1d82c200a78c97769dc72f285c638d382530e3
TheGreatOath <pertuisance@gmail.com>
2025-08-20 00:43:18 +0300

    Update Script.lua

--- Tower of Hell/Script.lua
+++ Tower of Hell/Script.lua
@@ -1,1 +1,40 @@
+local Worm = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGreatOath/Worm/refs/heads/main/InterfaceManager.lua"))()
 
+local Window = Worm:CreateWindow({
+    Title = "Worm",
+    SubTitle = "- Tower of Hell",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "home" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Worm.Options
+
+do
+    local Section = Tabs.Main:AddSection("Auto Farm")
+
+        
+
+end
+
+SaveManager:SetLibrary(Worm)
+InterfaceManager:SetLibrary(Worm)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("WormScriptHub")
+SaveManager:SetFolder("WormScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+Window:SelectTab(1)
+
+SaveManager:LoadAutoloadConfig()
